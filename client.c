fdf145686ebf53e3d3bb01956fbc18359c4db326
saidyanak <said.ynk@gmail.com>
2025-01-30 20:44:58 +0300

    bitti sanırım ama eminde değilim şimdi

---
+++ client.c
@@ -0,0 +1,79 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int	ft_atoi(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (*str)
+	{
+		index = index * 10 + (*str - '0');
+		str++;
+	}
+	return (index);
+}
+
+void	ft_send_pid(int pid)
+{
+	int	i;
+	int	client_pid;
+
+	client_pid = getpid();
+	i = 0;
+	while (i < 32)
+	{
+		if (client_pid >> i & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		i++;
+	}
+}
+
+void	ft_send(int pid, unsigned char send)
+{
+	int				i;
+	unsigned char	tmp;
+
+	i = 0;
+	while (i < 8)
+	{
+		if (send >> i & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		i++;
+	}
+}
+
+void	ft_handle(int sig)
+{
+	if (sig == SIGUSR1)
+	{
+		write(1, "yazdım", 7);
+	}
+}
+
+int	main(int av, char **ag)
+{
+	int	pid;
+
+	if (av == 3)
+	{
+		signal(SIGUSR1, ft_handle);
+		pid = ft_atoi(ag[1]);
+		if (pid == 0)
+			return (write(1,
+					"Please enter correct pid!Like: ./client 28452Hellor world",
+					57), 0);
+		ft_send_pid(pid);
+		while (*(ag[2]))
+		{
+			ft_send(pid, *(ag[2]));
+			*(ag[2])++;
+		}
+		ft_send(pid, '\0');
+	}
+}
---
+++ server.c
@@ -0,0 +1,67 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int		client_pid = 0;
+
+void	ft_putnbr(int n)
+{
+	if (n > 9)
+		ft_putnbr(n / 10);
+	write(1, &"0123456789"[n % 10], 1);
+}
+
+int	ft_write_message(int client_signal)
+{
+	static int	bit_message = 1;
+	static int	message = 0;
+
+	if (client_signal == SIGUSR1)
+		message += bit_message;
+	bit_message *= 2;
+	if (bit_message == 256)
+	{
+		write(1, &message, 1);
+		bit_message = 1;
+		if (message == 0)
+			return (write(1, "\n", 1), 1);
+		message = 0;
+	}
+	return (0);
+}
+
+void	ft_handler(int client_signal)
+{
+	static int	bit_pid = 0;
+	int			control;
+
+	control = 0;
+	if (bit_pid < 32)
+	{
+		if (client_signal == SIGUSR1)
+			client_pid += 1 << bit_pid;
+		bit_pid++;
+	}
+	else
+	{
+		control = ft_write_message(client_signal);
+		if (control)
+		{
+			kill(client_pid, SIGUSR1);
+			bit_pid = 0;
+			client_pid = 0;
+		}
+	}
+}
+
+int	main(void)
+{
+	ft_putnbr(getpid());
+	write(1, "\n", 1);
+	signal(SIGUSR1, ft_handler);
+	signal(SIGUSR2, ft_handler);
+	while (1)
+	{
+	}
+	return (0);
+}
