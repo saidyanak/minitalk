2b7da9c7bbaaea1a4cc50b59e34c1682e599733c
saidyanak <said.ynk@gmail.com>
2026-02-02 11:12:45 +0300

    fix: norm and 25 line, add: REAMDE.md

--- client.c
+++ client.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syanak <syanak@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/02 10:34:34 by syanak            #+#    #+#             */
+/*   Updated: 2026/02/02 11:11:21 by syanak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <signal.h>
 #include <unistd.h>
 
@@ -22,7 +34,7 @@ void	handle_signal(int sig)
 	g_flag = 1;
 }
 
-void	send_client_pid(int client_pid, int server_pid)
+int	send_client_pid(int client_pid, int server_pid)
 {
 	int	bit;
 
@@ -31,47 +43,49 @@ void	send_client_pid(int client_pid, int server_pid)
 	{
 		bit--;
 		if (client_pid & (1 << bit))
-			kill(server_pid, SIGUSR1);
+		{
+			if (kill(server_pid, SIGUSR1) == -1)
+				return (1);
+		}
 		else
-			kill(server_pid, SIGUSR2);
+		{
+			if (kill(server_pid, SIGUSR2) == -1)
+				return (1);
+		}
 		usleep(20);
 	}
 }
 
-void	send_message(char *message, int server_pid)
+void	send_byte(char byte, int server_pid)
 {
 	int	i;
 
-	while (*message)
-	{
-		i = 8;
-		while (i)
-		{
-			i--;
-			g_flag = 0;
-			if (*message & (1 << i))
-				kill(server_pid, SIGUSR1);
-			else
-				kill(server_pid, SIGUSR2);
-			while (!g_flag)
-				pause();
-		}
-		message++;
-	}
 	i = 8;
-	while (--i > 0)
+	while (i)
 	{
-		kill(server_pid, SIGUSR2);
+		i--;
 		g_flag = 0;
+		if (byte & (1 << i))
+		{
+			if (kill(server_pid, SIGUSR1) == -1)
+				return (1);
+		}
+		else
+		{
+			if (kill(server_pid, SIGUSR2) == -1)
+				return (1);
+		}
 		while (!g_flag)
-				pause();
+			;
 	}
 }
 
 int	main(int ac, char **av)
 {
 	int	server_pid;
+	int	i;
 
+	i = 0;
 	if (ac != 3)
 	{
 		write(2, "Usage: ./client <pid> <message>\n", 31);
@@ -82,6 +96,11 @@ int	main(int ac, char **av)
 	server_pid = ft_atoi(av[1]);
 	g_flag = 0;
 	send_client_pid(getpid(), server_pid);
-	send_message(av[2], server_pid);
+	while (av[2][i])
+	{
+		send_byte(av[2][i], server_pid);
+		i++;
+	}
+	send_byte(0, server_pid);
 	return (0);
 }
